dbe26d0f7ede0a6a92f75d029709c11a01633167
dallten7-spec <dallten7@gmail.com>
2026-08-28 08:22:57 +0330

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://vqjcfohjfsvdzylgvywp.supabase.co';
-const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
+const supabaseAnonKey = 'sb_publishable_3WxoJr7UnrRVK3X7q2onaA_v7XhKIer';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
